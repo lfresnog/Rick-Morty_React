25bc36b3f4fc9c3f362619dfe30c8c30974dd9f3
lfresnog <luisfresno99@gmail.com>
2020-02-14 15:47:00 +0100

    First

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Data from "./assets/data";
+import Character from "./components/Character";
+import "./components/styles.css";
+
+class App extends Component {
+  state = {
+    data: { ...Data },
+    alive: null
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="content">
+          <div className="title"> THE RICK AND MORTY</div>
+          <div className="main_content">
+            {this.state.data.results.map(item => {
+              return <Character key={item.id} characterInfo={item} />;
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
